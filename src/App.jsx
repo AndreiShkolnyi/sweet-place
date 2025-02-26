eda003e3a1a57f38a319bfc0e3d37c777950e9d7
Andrei Shkolnyi <ashkolny@klerk.ru>
2025-02-26 14:12:17 +0300

    added reqct-query client

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { GlobalStyles } from "./styles/GlobalStyles";
+
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
@@ -7,9 +12,7 @@ import PageNotFound from "./pages/PageNotFound";
 import Settings from "./pages/Settings";
 import NewUsers from "./pages/Users";
 import Account from "./pages/Account";
-import { GlobalStyles } from "./styles/GlobalStyles";
 import AppLayout from "./ui/layouts/AppLayout";
-import { QueryClient } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,7 +24,8 @@ const queryClient = new QueryClient({
 
 const App = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -38,7 +42,7 @@ const App = () => {
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 };
 
